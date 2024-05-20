c630e685897aaa29dd67769519b271de3b4ea8f5
RyantheKing <48844450+RyantheKing@users.noreply.github.com>
2024-05-19 19:04:39 -0700

    update assignment 3

--- asgn3/src/triangle.js
+++ asgn3/src/triangle.js
@@ -1,41 +1,3 @@
-class Triangle {
-    constructor() {
-        this.type = 'triangle';
-        this.position = [0.0, 0.0, 0.0];
-        this.color = [1.0, 1.0, 1.0, 1.0];
-        this.size = 5.0;
-    }
-
-    render() {
-        let xy = this.position;
-        let rgba = this.color;
-        let size = this.size;
-
-        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-
-        gl.uniform1f(u_Size, size);
-
-        let d = size/200.0;
-        drawTriangle([xy[0], xy[1], xy[0]+d, xy[1], xy[0], xy[1]+d]);
-    }
-}
-
-function drawTriangle(vertices) {
-    let n = 3;
-
-    let vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-        console.log('Failed to create the buffer object');
-        return -1;
-    }
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(a_Position);
-    gl.drawArrays(gl.TRIANGLES, 0, n);
-}
-
 function drawTriangle3D(vertices) {
     let n = 3
 
@@ -73,4 +35,37 @@ function drawTriangle3DUV(vertices, uv) {
     gl.enableVertexAttribArray(a_UV);
     
     gl.drawArrays(gl.TRIANGLES, 0, n);
+}
+
+function drawCircle3D(center, radius, segments) {
+    // let vertices = [center[0], center[1], center[2]];
+    let rad_step = 2 * Math.PI / segments;
+    // let circleBuffer = gl.createBuffer();
+    // if (!circleBuffer) {
+    //     console.log('Failed to create the buffer object');
+    //     return -1;
+    // }
+
+    for (let i = 0; i < segments; i++) {
+        drawTriangle3D([center[0], center[1], center[2], 
+                        center[0] + Math.cos(rad_step * i) * radius, center[1] + Math.sin(rad_step * i) * radius, center[2],
+                        center[0] + Math.cos(rad_step * (i + 1)) * radius, center[1] + Math.sin(rad_step * (i + 1)) * radius, center[2]]);
+    }
+        
+
+    // for (let i = 0; i < segments; i++) {
+    //     vertices.push(center[0] + Math.cos(rad_step * i) * radius);
+    //     vertices.push(center[1] + Math.sin(rad_step * i) * radius);
+    //     vertices.push(center[2]);
+    // }
+    // vertices.push(center[0] + radius);
+    // vertices.push(center[1]);
+    // vertices.push(center[2]);
+
+    // gl.bindBuffer(gl.ARRAY_BUFFER, circleBuffer);
+    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+    // gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+    // gl.enableVertexAttribArray(a_Position);
+
+    // gl.drawArrays(gl.TRIANGLE_FAN, 0, segments + 2);
 }
